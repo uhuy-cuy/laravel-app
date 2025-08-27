ba383730d4cd750bed5619c10330854b2dd7e920
uhuy-cuy <uhuycuy0000@gamil.com>
2025-08-27 13:38:37 +0700

    baru

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light d-flex align-items-center justify-content-center" style="height: 100vh;">
 
 <div class="card shadow-lg p-4" style="width: 400px;">
-    <h4 class="text-center mb-3">Login</h4>
+    <h4 class="text-center mb-3">Login1</h4>
     
     @if ($errors->any())
         <div class="alert alert-danger">
